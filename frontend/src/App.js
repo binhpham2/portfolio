12a5ebc31e5e2bbc6c67d1582c4a2c788e1c93b6
Binh Pham <binh.pham5918@gmail.com>
2025-01-12 22:19:19 +0200

    credit notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Background from './components/background/Background';
 import Welcome from './components/welcome/Welcome';
 import MenuFrame from './components/menu/MenuFrame';
+import CreditNote from './components/credit/CreditNote';
 
 const App = () => {
   return (
     <div>
       <Background/>
       <Welcome/>
-      <MenuFrame/>
+      
+      <CreditNote/>
     </div>
   );
 }
---
+++ frontend/src/components/credit/CreditNote.jsx
@@ -0,0 +1,11 @@
+import './credit.css'
+
+const CreditNote = () => {
+    return (
+        <p className='CreditNote'>
+            Art by DALL-E. Animation by me.
+        </p>
+    );
+}
+
+export default CreditNote
