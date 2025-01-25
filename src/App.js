22ce9c32d0da7e40dcbb9f45d849511d526df636
Binh Pham <binh.pham5918@gmail.com>
2025-01-25 20:22:53 +0200

    remove tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Background from './components/background/Background'
+import Welcome from './components/welcome/Welcome'
 
 const App = () => {
   return (
     <div>
+      <Background />
+      <Welcome />
     </div>
   );
 }
